b9a8b1f5815e6f6ce34b1a848e78bb4634eb9672
CharliePearce <miss.neophyte@gmail.com>
2015-11-01 15:46:25 +0000

    password function added

--- StreetArtSnaps/app/controllers/photos_controller.rb
+++ StreetArtSnaps/app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  # before_action :authtenticate_user!, except: [:index, :show]
 
   # GET /photos
   # GET /photos.json
--- StreetArtSnaps/app/models/user.rb
+++ StreetArtSnaps/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :trackable, :validatable
+  has_many :images, dependent: :destroy
+
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 end
