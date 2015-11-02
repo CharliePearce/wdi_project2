02bf8d2fff29d3473492f66b4b4f315c3a92d9b3
CharliePearce <miss.neophyte@gmail.com>
2015-11-02 14:12:03 +0000

    name added to locations

--- street_art_snaps/db/seeds.rb
+++ street_art_snaps/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+[{name: "Berlin, Germany"}, {name: "Lisbon, Portugal"}, {name: "Dublin, Ireland"}, {name: "London, UK"}, {name: "Bristol, UK"}, {name: "Paris, France"}, {name: "Cologne, Germany"}, {name: "Ghent, Belgium"}, {name: "Rotterdam, The Netherlands"}, {name: "Milan, Italy"}, {name: "Marseille, France"}, {name: "Porto, Portugal"} ].each do |loc_hash|
+
+  Location.create(loc_hash)
+end
