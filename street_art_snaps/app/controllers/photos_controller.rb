2360b4d0796bfb40d00010a2485d0366f027a14b
CharliePearce <miss.neophyte@gmail.com>
2015-11-03 14:41:20 +0000

    image uploader working

--- street_art_snaps/app/controllers/photos_controller.rb
+++ street_art_snaps/app/controllers/photos_controller.rb
@@ -71,6 +71,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:title, :description, :location_id)
+      params.require(:photo).permit(:title, :description, :image, :location_id)
     end
 end
