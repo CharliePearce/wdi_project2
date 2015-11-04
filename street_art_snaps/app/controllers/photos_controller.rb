8ba3483fedd5ed23c6c4259c315bce8cc2073c53
CharliePearce <miss.neophyte@gmail.com>
2015-11-04 09:45:12 +0000

    about to delete migration AddAboutMeToUsers

--- street_art_snaps/app/controllers/photos_controller.rb
+++ street_art_snaps/app/controllers/photos_controller.rb
@@ -1,6 +1,7 @@
 class PhotosController < ApplicationController
-  before_action :set_photo, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+
 
   def upvote
     @photo = Photo.find(params[:id])
