f0020e3c88513fcbb4658fd88dcf1548ac3b020a
CharliePearce <miss.neophyte@gmail.com>
2015-11-04 11:24:32 +0000

    carrierwave working for user_image, about to install rmagick for user_image

--- street_art_snaps/app/models/user.rb
+++ street_art_snaps/app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :photos, dependent: :destroy
   belongs_to :location
+  mount_uploader :user_image, UserImageUploader
 
   def full_name
     [first_name, last_name].join(" ")
